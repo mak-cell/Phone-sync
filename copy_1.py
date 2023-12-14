db3be20e3785821f419a2e01df4d5780a08e067d
Mohan Krushna <mohankrushnabehera@gmail.com>
2023-12-14 09:56:22 +0530

    logger enabled

--- copy_1.py
+++ copy_1.py
@@ -1,71 +1,98 @@
 import os
 from ftplib import FTP
-from tqdm import tqdm  # Import tqdm for progress tracking
+from tqdm import tqdm
+import logging
+from datetime import datetime
 
-ftp_server = "192.168.101.2"
+# Configure logging
+log_folder = "logs"  # Change this to your desired log folder
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+
+log_filename = f"{datetime.now().strftime('%Y%m%d_%H%M%S')}_logs.log"
+log_filepath = os.path.join(log_folder, log_filename)
+
+logging.basicConfig(filename=log_filepath, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+ftp_server = "192.168.101.3"
 ftp_port = 2221
 ftp_username = "android"
 ftp_password = "android"
-
 def copy(local_folder, remote_folders):
     # Initialize the overall progress bar
     overall_progress = None
     total_files = 0
 
-    for remote_folder in remote_folders:
-        ftp = FTP()
-        ftp.connect(ftp_server, ftp_port)
-        ftp.login(ftp_username, ftp_password)
-        try:
-            ftp.cwd(remote_folder)
-            file_list = []
-            ftp.retrlines("LIST", file_list.append)
-            total_files += sum(1 for item in file_list if (item.split()[-1]).endswith((".jpg", ".png")))
-        except Exception as e:
-            print(f"Error while accessing '{remote_folder}': {str(e)}")
-        finally:
-            ftp.quit()
+    # Set up FTP connection outside the loop
+    ftp = FTP()
+    ftp.connect(ftp_server, ftp_port)
+    ftp.login(ftp_username, ftp_password)
 
-    overall_progress = tqdm(total=total_files, unit="B", unit_scale=True, unit_divisor=1024, desc="Copying")
+    try:
+        for remote_folder in remote_folders:
+            try:
+                ftp.cwd(remote_folder)
+                file_list = []
+                ftp.retrlines("LIST", file_list.append)
+                total_files += sum(1 for item in file_list if (item.split()[-1]).endswith((".jpg", ".png", "mp4")))
+            except Exception as e:
+                logger.error(f"Error while accessing '{remote_folder}': {str(e)}")
+                continue  # Skip to the next folder if it does not exist
 
-    for remote_folder in remote_folders:
-        try:
-            # Connect to the FTP server
-            ftp = FTP()
-            ftp.connect(ftp_server, ftp_port)
-            ftp.login(ftp_username, ftp_password)
+        # Create the local folder if it doesn't exist
+        local_folder_path = local_folder
+        if not os.path.exists(local_folder_path):
+            os.makedirs(local_folder_path)
 
-            # Attempt to change to the remote folder
-            ftp.cwd(remote_folder)
+        overall_progress = tqdm(total=total_files, unit="B", unit_scale=True, unit_divisor=1024, desc="Copying")
 
-            # Create the local folder if it doesn't exist
-            local_folder_path = local_folder
-            if not os.path.exists(local_folder_path):
-                os.makedirs(local_folder_path)
+        local_videos_folder = os.path.join(local_folder, "Videos")
 
-            # List the files in the remote folder
-            file_list = []
-            ftp.retrlines("LIST", file_list.append)
+        # Create a "Videos" folder if it doesn't exist in the local folder
+        if not os.path.exists(local_videos_folder):
+            os.makedirs(local_videos_folder)
 
-            # Get a list of filenames already in the local folder
-            local_files = os.listdir(local_folder_path)
+        for remote_folder in remote_folders:
+            try:
+                ftp = FTP()
+                ftp.connect(ftp_server, ftp_port)
+                ftp.login(ftp_username, ftp_password)
+                ftp.cwd(remote_folder)
+                file_list = []
+                ftp.retrlines("LIST", file_list.append)
+                # Get a list of filenames already in the local videos folder
+                local_video_files = os.listdir(local_videos_folder)
 
-            # Iterate over the remote files and download each one if it doesn't already exist locally
-            for item in file_list:
-                parts = item.split()
-                filename = parts[-1]
-                if filename.endswith(".jpg") or filename.endswith(".png"):
+                # Get a list of filenames already in the local folder
+                local_files = os.listdir(local_folder_path)
+
+                for item in file_list:
+                    parts = item.split()
+                    filename = parts[-1]
                     local_file_path = os.path.join(local_folder_path, filename)
-                    if filename not in local_files:
-                        with open(local_file_path, 'wb') as local_file:
-                            ftp.retrbinary("RETR " + filename, local_file.write)
-                    overall_progress.update(1)  # Increment overall progress
 
-        except Exception as e:
-            print(f"Error while copying from '{remote_folder}': {str(e)}")
+                    if filename.endswith((".jpg", ".png")):
+                        if filename not in local_files:
+                            with open(local_file_path, 'wb') as local_file:
+                                ftp.retrbinary("RETR " + filename, local_file.write)
+                            logger.info(f"Copied: {filename}")
+                            overall_progress.update(1)  # Increment overall progress
+                        
 
-        finally:
-            ftp.quit()
+                    elif filename.endswith(".mp4"):
+                        local_file_path = os.path.join(local_videos_folder, filename)
+                        if filename not in local_video_files:
+                            with open(local_file_path, 'wb') as local_file:
+                                ftp.retrbinary("RETR " + filename, local_file.write)
+                            logger.info(f"Copied: {filename}")
+                            overall_progress.update(1)  # Increment overall progress
+                            
 
-    overall_progress.close()  # Close the overall progress bar
+            except Exception as e:
+                logger.error(f"Error while copying from '{remote_folder}': {str(e)}")
 
+        overall_progress.close()  # Close the overall progress bar
+
+    finally:
+        ftp.quit()
--- foldermapping.py
+++ foldermapping.py
@@ -12,14 +12,14 @@ remote_phone_download = [phone_download, phone_donwload_insta, phone_download_se
 local_phone_download = """D:/Photos/pixel6A"""
 
 
-phone_camera_photos = """DCIM/Camera"""
-phone_Google_photos = """DCIM/Google Photos"""
-phone_Snapseed_photos = """DCIM/Snapseed"""
-phone_Screenshots1 = """DCIM/Screenshots"""
-phone_Screenshots2 = """Pictures/Screenshots"""
-phone_picture = """Pictures"""
-insta_backups_photos = """Pictures/Instagram"""
-AdobeLightRoom_edited_photos = """Pictures/AdobeLightroom"""
+phone_camera_photos = "DCIM/Camera"
+phone_Google_photos = "DCIM/Google Photos"
+phone_Snapseed_photos = "DCIM/Snapseed"
+phone_Screenshots1 = "DCIM/Screenshots"
+phone_Screenshots2 = "Pictures/Screenshots"
+phone_picture = "Pictures"
+insta_backups_photos = "Pictures/Instagram"
+AdobeLightRoom_edited_photos = "Pictures/AdobeLightroom"
 remote_PhoneMedia = [phone_camera_photos,phone_Google_photos,phone_Snapseed_photos,phone_Screenshots1,phone_Screenshots2,phone_picture,insta_backups_photos,AdobeLightRoom_edited_photos]
 local_phoneMedia = """D:/Photos/pixel6A"""
 
